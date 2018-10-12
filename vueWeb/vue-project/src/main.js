c0e1903749a20db3b21fcfe013fcc18c81679730
rapdan <rapdan@gmail.com>
2018-10-12 13:27:37 +0200

    Vue component

--- vueWeb/vue-project/src/main.js
+++ vueWeb/vue-project/src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+Vue.component('my-counter', {
+  template: `
+  <div>
+    <h3>Ilość: {{ counter }}</h3>
+    <button @click="add(1)">+1</button>
+    <button @click="add(-1)">-1</button>
+  </div>
+  `,
+  data: function(){
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    add: function(num) {
+      this.counter+=(num)
+    }
+  }
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  // render: h => h(App)
 })
